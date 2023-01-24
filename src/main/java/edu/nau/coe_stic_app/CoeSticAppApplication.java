c6e3abe485bb92c861ab0b93647c760ef37c418d
Sam Gerstner <samgerstner1222@gmail.com>
2023-01-23 18:00:28 -0700

    Add Spring Initializr Template
    Signed-off-by: Sam Gerstner <samgerstner1222@gmail.com>

---
+++ src/main/java/edu/nau/coe_stic_app/CoeSticAppApplication.java
@@ -0,0 +1,13 @@
+package edu.nau.coe_stic_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoeSticAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoeSticAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nau/coe_stic_app/CoeSticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nau.coe_stic_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoeSticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
