5be76f69619b42041f93e13e9d08d0c9f4dfafd5
Sam Gerstner <samgerstner1222@gmail.com>
2023-01-28 16:14:33 -0700

    Add Data Retrierval/Passthrough
    Modifies the dashboard endpoint to obtain all the requirement instances from the database. The call to getRequirementInstances() will need to be updated once Spring Security is implemented.

--- src/main/java/edu/nau/coe_stic_app/controllers/StudentController.java
+++ src/main/java/edu/nau/coe_stic_app/controllers/StudentController.java
@@ -1,9 +1,13 @@
 package edu.nau.coe_stic_app.controllers;
 
+import edu.nau.coe_stic_app.MySQL_Helper;
+import edu.nau.coe_stic_app.models.RequirementInstance;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import java.sql.Connection;
+import java.util.List;
 
 @Controller
 public class StudentController
@@ -11,6 +15,11 @@ public class StudentController
     @RequestMapping(path = {"/", "/home", "/dashboard", "/student"}, method = RequestMethod.GET)
     public String studentDashboard(Model model)
     {
+
+        Connection con = MySQL_Helper.createSqlConnection();
+        List<RequirementInstance> instances = MySQL_Helper.getRequirementInstances(con, "changeme");
+        model.addAttribute("req_instances", instances.toArray());
+
         return "student";
     }
 }
