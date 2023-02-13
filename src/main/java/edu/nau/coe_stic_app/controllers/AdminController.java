006b4308c0d1b3802f3f778396729356b98626cb
Sam Gerstner <samgerstner1222@gmail.com>
2023-02-13 14:50:01 -0700

    Refactor SQL
    Remove MySQL_Helper class and add TODOs for calls to doc API for database information. Decision was made to have all DB calls in the API instead of the web app.

--- src/main/java/edu/nau/coe_stic_app/controllers/AdminController.java
+++ src/main/java/edu/nau/coe_stic_app/controllers/AdminController.java
@@ -1,12 +1,10 @@
 package edu.nau.coe_stic_app.controllers;
 
-import edu.nau.coe_stic_app.MySQL_Helper;
 import edu.nau.coe_stic_app.models.RequirementInstance;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.sql.Connection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,13 +14,12 @@ public class AdminController
     @RequestMapping(path = {"/admin", "/admin/home", "/admin/dashboard"}, method = RequestMethod.GET)
     public String adminDashboard(Model model)
     {
-        Connection con = MySQL_Helper.createSqlConnection();
-        List<String> users = MySQL_Helper.getAllApprovedUsers(con);
+        //TODO: Insert call to doc API to get a list of all the approved student users
         HashMap<String, List<RequirementInstance>> userRequirements = new HashMap<String, List<RequirementInstance>>();
 
         for(String user : users)
         {
-            List<RequirementInstance> requirements = MySQL_Helper.getRequirementInstances(con, user);
+            //TODO: Insert call to doc API to get a list of all req instances for specific user
             userRequirements.put(user, requirements);
         }
 
--- src/main/java/edu/nau/coe_stic_app/controllers/StudentController.java
+++ src/main/java/edu/nau/coe_stic_app/controllers/StudentController.java
@@ -1,13 +1,10 @@
 package edu.nau.coe_stic_app.controllers;
 
-import edu.nau.coe_stic_app.MySQL_Helper;
 import edu.nau.coe_stic_app.models.RequirementInstance;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.sql.Connection;
-import java.util.List;
 
 @Controller
 public class StudentController
@@ -16,8 +13,7 @@ public class StudentController
     public String studentDashboard(Model model)
     {
 
-        Connection con = MySQL_Helper.createSqlConnection();
-        List<RequirementInstance> instances = MySQL_Helper.getRequirementInstances(con, "changeme");
+        //TODO: Insert call to doc API to get all req instances for specific UID
         model.addAttribute("req_instances", instances.toArray());
 
         return "student";
